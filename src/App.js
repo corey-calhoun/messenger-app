fabb97da6aa3ddd6b3fc4d90ac699a1eb6630fbe
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-07 15:14:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Messenger App</h1>
+
+      {/* Input Field */}
+      <input value={input} onChange={event => setInput(event.target.value)} />
+
+      {/* Button */}
+      <button>Send Message</button>
+
+      {/* Messages Themselves */}
+
     </div>
   );
 }
